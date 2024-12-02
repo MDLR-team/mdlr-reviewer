83454e97cda0573ed6f497079359688f8d0da5b4
ilia-kuzmin-xyz <markkabierski@gmail.com>
2024-12-02 23:03:53 +0000

    Added gen-result-service

--- packages/project/src/services/project-service/project-service.ts
+++ packages/project/src/services/project-service/project-service.ts
@@ -19,6 +19,7 @@ export class ProjectService implements IProjectService {
   private dbService: DBService;
 
   private supabaseClient: any;
+  private chatGptConfig?: ChatGptConfig;
 
   constructor(
     id: string,
@@ -33,6 +34,7 @@ export class ProjectService implements IProjectService {
 
     // Initialize Supabase client
     this.supabaseClient = this._initializeSupabase(config.supabase);
+    this.chatGptConfig = config.chatGpt;
 
     this.noteService = new NoteService(this);
     this.summaryService = new SummaryService(this);
--- packages/ui-kit/src/components/panel/RightSummary/RightSummary.tsx
+++ packages/ui-kit/src/components/panel/RightSummary/RightSummary.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Typography, Box } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 interface RightSummaryProps {
   activeSummary: any | null;
@@ -17,9 +20,23 @@ export const RightSummary: React.FC<RightSummaryProps> = ({
   }
 
   return (
-    <Box>
-      <h1>{activeSummary.title}</h1>
-      <Box>{activeSummary.content}</Box>
+    <Box
+      sx={{
+        padding: "var(--mr-gap-l)",
+        overflowY: "auto",
+      }}
+    >
+      <h1 style={{ paddingBottom: "var(--mr-gap-l)" }}>
+        {activeSummary.title}
+      </h1>
+
+      <Box>
+        <ReactMarkdown
+          children={activeSummary.content}
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+        />
+      </Box>
     </Box>
   );
 };
